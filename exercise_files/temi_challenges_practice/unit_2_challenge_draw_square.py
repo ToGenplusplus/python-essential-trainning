549585463fb2c4968ad0cf3a9d7f06c54b0037a1
temiowoaje <temiowoaje@carfax.com>
2022-10-07 07:01:18 -0400

    finished my implementation for challenge 2 of drawing a square

--- exercise_files/temi_challenges_practice/unit_2_challenge_draw_square.py
+++ exercise_files/temi_challenges_practice/unit_2_challenge_draw_square.py
@@ -12,28 +12,35 @@
 # going to have a draw sides of square method
 
 import sys
+import time
+from termcolor import colored, cprint
 
 class DrawSquare:
     def __init__(self, unit):
-        if type(unit is not int) or unit < 1:
-            print("Invalid unit value, expecting int >= 1")
+        if type(unit) is not int or unit < 1:
+            cprint("Unit: {}, Unit type: {}".format(unit, type(unit)), "red", attrs=["bold"])
+            cprint("Invalid unit value, expecting int >= 1", "red", attrs=["bold"])
             sys.exit(1)
         self.unit = unit
 
     def Draw(self):
         if (self.unit == 1):
-            print("Haha clever a 1 unit square. Here you go: []")
+            cprint("Haha clever a 1 unit square. Here you go: []", "yellow")
         else:
+            
             self.DrawSquareWithSpecifiedUnitSize()
 
     def DrawSquareWithSpecifiedUnitSize(self):
+        time.sleep(1)
+        cprint("DRAWING SQUARE A {} UNIT SQUARE...".format(self.unit), "green", attrs=["bold"])
         self.DrawBase()
         self.DrawSides()
         self.DrawBase()
 
     def DrawBase(self):
         base = "* " * self.unit
-        print(base)
+        time.sleep(1)
+        print(colored(base, "yellow", attrs=["bold"]))
 
     def DrawSides(self):
         # conditions for the sides
@@ -45,6 +52,12 @@ class DrawSquare:
             num_spaces_between_sides = (self.unit * 2) - 3
             num_vertical_hashes = self.unit - 2
 
-            side = "*" + "" * num_spaces_between_sides + "*"
+            side = "*" + " " * num_spaces_between_sides + "*"
             for i in range(num_vertical_hashes):
-                print(side)
+                time.sleep(0.5)
+                print(colored(side, "yellow", attrs=["blink"]))
+
+
+for i in range(2, 6):
+    draw_square = DrawSquare(i)
+    draw_square.Draw()
